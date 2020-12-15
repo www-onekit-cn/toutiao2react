0e15fd2310c502d996d677143215c758da3c769d
WangYeWei <826036140@qq.com>
2020-12-15 11:23:52 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import tt from './toutiao2react/tt'
-import Text from './toutiao2react/ui/text/index'
+// import Text from './toutiao2react/ui/text/index'
+import RichText from './toutiao2react/ui/rich-text/index'
 
 type Myprops = {
   value: String
@@ -10,7 +11,19 @@ class App extends React.Component<Myprops, any> {
   constructor(prop: any) {
     super(prop)
     this.state = {
-      value: '这是王野未写的TypeScript'
+      value: '这是王野未写的TypeScript',
+      htmlsnip: `<h1>王野未</h1>`,
+      nodes: [{
+        name: 'div',
+        attrs: {
+          class: 'div_class',
+          style: 'line-height: 60px; color: red;'
+        },
+        children: [{
+          type: 'text',
+          text: 'You never know what you\'re gonna get.'
+        }]
+      }]
     }
     this.handleCick = this.handleCick.bind(this)
   }
@@ -24,7 +37,7 @@ class App extends React.Component<Myprops, any> {
   render() {
     return (
       <div>
-        <Text decode={false}>{this.state.value}</Text>
+        <RichText nodes={this.state.nodes} />
         <button onClick={this.handleCick}>BUTTON</button>
       </div>
 
--- src/toutiao2react/ui/rich-text/rich-text.tsx
+++ src/toutiao2react/ui/rich-text/rich-text.tsx
@@ -1,14 +1,46 @@
-import { Component } from 'react'
+import onekit_bahavior from '../../behaviors/onekit_bahavior'
+import toutiao_behavior from '../../behaviors/toutiao_behavior'
+const oneutil: any = require('oneutil')
 
+@toutiao_behavior
+class RichText extends onekit_bahavior {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      html: ''
+    }
+  }
 
-class RichText extends Component<any, any> {
-  constructor(prop: any) {
-    super(prop)
+  componentDidMount() {
+    const resu = RichText.nodes2html(this.props.nodes)
+    this.setState({
+      html: resu
+    })
+  }
+
+  static getDerivedStateFromProps(props: any, state: any) {
+    if (RichText.nodes2html(props.nodes) !== state.html) {
+      return {
+        html: RichText.nodes2html(props.nodes)
+      }
+    }
+    return null
+  }
+
+  static nodes2html(state: any) {
+    const html = oneutil.STRING.replace(typeof (state) == 'string' ? state : oneutil.HTML.nodes2html(state))
+    return html
   }
 
   render() {
     return (
-      <></>
+      <>
+        <div
+          className={this.state.onekitClass}
+          id={this.state.onekitId}
+          dangerouslySetInnerHTML={{ __html: this.state.html }}>
+        </div>
+      </>
     )
   }
 }
